0dd32c0c693feb47088a437da1bd6e8833f98376
kiennd85 <duykien85@gmail.com>
2023-05-23 09:39:48 +0700

    update Light

--- Lesson5_Thuchanh1/bt/src/App.js
+++ Lesson5_Thuchanh1/bt/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 import CountNumber from './Screens/Lesson5/CountNumber';
 import Test1 from './Screens/Lesson5_test/test1';
 import Test2 from './Screens/Lesson5_test/test2';
 import Test3 from './Screens/Lesson5_test/test3';
+import Light from './Screens/Lesson5/Light';
 
 function App() {
-  return <Test3 />;
+  return <Light />;
 }
 
 export default App;

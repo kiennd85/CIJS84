cb4a6d22c84b97b3b0c885778e5071c5646d8e7e
kiennd85 <duykien85@gmail.com>
2023-05-28 14:49:24 +0700

    Update App.js

--- CIJS84/bt/src/App.js
+++ CIJS84/bt/src/App.js
@@ -1,6 +1,7 @@
 import CountNumber from './Screens/Lesson5/CountNumber';
 import Light from './Screens/Lesson5/Light';
 import Test5 from './Screens/Lesson5_test/test5.web3';
+
 import Register from './Screens/RegisterForm/RegisterForm';
 
 // function App() {

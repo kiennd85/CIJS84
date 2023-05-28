db28e7b8c36da57988053299623050f5b4c3671c
kiennd85 <duykien85@gmail.com>
2023-05-28 19:30:18 +0700

    Update test5.web3.jsx

--- CIJS84/bt/src/Screens/Lesson5_test/test5.web3.jsx
+++ CIJS84/bt/src/Screens/Lesson5_test/test5.web3.jsx
@@ -3,35 +3,180 @@ import { useState } from 'react';
 
 const Test5 = () => {
   const [balance, setBalance] = useState(0);
+  const [tokenBalance, settokenBalance] = useState(0);
 
-  const testnet = 'https://bsc-dataseed1.binance.org:443';
+  const network = 'https://bsc-dataseed1.binance.org:443';
   const walletAddress = '0x7755f284ecbb16c4fd58b947b6eba613ffeed3bb';
 
-  const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
+  const web3 = new Web3(new Web3.providers.HttpProvider(network));
 
-  //console.log(web3);
+  //USDT
+  const token_address = '0x55d398326f99059fF775485246999027B3197955';
+  const token_abi = [
+    { inputs: [], payable: false, stateMutability: 'nonpayable', type: 'constructor' },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, internalType: 'address', name: 'owner', type: 'address' },
+        { indexed: true, internalType: 'address', name: 'spender', type: 'address' },
+        { indexed: false, internalType: 'uint256', name: 'value', type: 'uint256' },
+      ],
+      name: 'Approval',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, internalType: 'address', name: 'previousOwner', type: 'address' },
+        { indexed: true, internalType: 'address', name: 'newOwner', type: 'address' },
+      ],
+      name: 'OwnershipTransferred',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        { indexed: true, internalType: 'address', name: 'from', type: 'address' },
+        { indexed: true, internalType: 'address', name: 'to', type: 'address' },
+        { indexed: false, internalType: 'uint256', name: 'value', type: 'uint256' },
+      ],
+      name: 'Transfer',
+      type: 'event',
+    },
+    { constant: true, inputs: [], name: '_decimals', outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }], payable: false, stateMutability: 'view', type: 'function' },
+    { constant: true, inputs: [], name: '_name', outputs: [{ internalType: 'string', name: '', type: 'string' }], payable: false, stateMutability: 'view', type: 'function' },
+    { constant: true, inputs: [], name: '_symbol', outputs: [{ internalType: 'string', name: '', type: 'string' }], payable: false, stateMutability: 'view', type: 'function' },
+    {
+      constant: true,
+      inputs: [
+        { internalType: 'address', name: 'owner', type: 'address' },
+        { internalType: 'address', name: 'spender', type: 'address' },
+      ],
+      name: 'allowance',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      constant: false,
+      inputs: [
+        { internalType: 'address', name: 'spender', type: 'address' },
+        { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      ],
+      name: 'approve',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    { constant: true, inputs: [{ internalType: 'address', name: 'account', type: 'address' }], name: 'balanceOf', outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }], payable: false, stateMutability: 'view', type: 'function' },
+    { constant: false, inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }], name: 'burn', outputs: [{ internalType: 'bool', name: '', type: 'bool' }], payable: false, stateMutability: 'nonpayable', type: 'function' },
+    { constant: true, inputs: [], name: 'decimals', outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }], payable: false, stateMutability: 'view', type: 'function' },
+    {
+      constant: false,
+      inputs: [
+        { internalType: 'address', name: 'spender', type: 'address' },
+        { internalType: 'uint256', name: 'subtractedValue', type: 'uint256' },
+      ],
+      name: 'decreaseAllowance',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    { constant: true, inputs: [], name: 'getOwner', outputs: [{ internalType: 'address', name: '', type: 'address' }], payable: false, stateMutability: 'view', type: 'function' },
+    {
+      constant: false,
+      inputs: [
+        { internalType: 'address', name: 'spender', type: 'address' },
+        { internalType: 'uint256', name: 'addedValue', type: 'uint256' },
+      ],
+      name: 'increaseAllowance',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    { constant: false, inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }], name: 'mint', outputs: [{ internalType: 'bool', name: '', type: 'bool' }], payable: false, stateMutability: 'nonpayable', type: 'function' },
+    { constant: true, inputs: [], name: 'name', outputs: [{ internalType: 'string', name: '', type: 'string' }], payable: false, stateMutability: 'view', type: 'function' },
+    { constant: true, inputs: [], name: 'owner', outputs: [{ internalType: 'address', name: '', type: 'address' }], payable: false, stateMutability: 'view', type: 'function' },
+    { constant: false, inputs: [], name: 'renounceOwnership', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function' },
+    { constant: true, inputs: [], name: 'symbol', outputs: [{ internalType: 'string', name: '', type: 'string' }], payable: false, stateMutability: 'view', type: 'function' },
+    { constant: true, inputs: [], name: 'totalSupply', outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }], payable: false, stateMutability: 'view', type: 'function' },
+    {
+      constant: false,
+      inputs: [
+        { internalType: 'address', name: 'recipient', type: 'address' },
+        { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      ],
+      name: 'transfer',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      constant: false,
+      inputs: [
+        { internalType: 'address', name: 'sender', type: 'address' },
+        { internalType: 'address', name: 'recipient', type: 'address' },
+        { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      ],
+      name: 'transferFrom',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    { constant: false, inputs: [{ internalType: 'address', name: 'newOwner', type: 'address' }], name: 'transferOwnership', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function' },
+  ];
 
-  async function get_balance() {
-    let bal = await web3.eth.getBalance(walletAddress);
-    bal = bal / 10 ** 18;
-    return bal;
+  const token_contract = new web3.eth.Contract(token_abi, token_address);
+
+  async function get_token_balance() {
+    let balance = await token_contract.methods.balanceOf(walletAddress).call();
+    balance = balance / 10 ** 18;
+    return balance;
   }
 
-  //   get_balance().then(function (result) {
-  //     setBalance(result);
-  //   });
+  async function get_balance() {
+    let balance = await web3.eth.getBalance(walletAddress);
+    balance = balance / 10 ** 18;
+    return balance;
+  }
 
-  const handleClick = async (e) => {
-    get_balance().then((result) => {
-      setBalance(result);
-    });
+  const handleClick = (e, token) => {
+    if (token == 'usdt') {
+      get_token_balance().then((result) => {
+        settokenBalance(result);
+      });
+    } else if (token == 'coin') {
+      get_balance().then((result) => {
+        setBalance(result);
+      });
+    }
   };
 
   return (
     <div>
       <div> Test Web3</div>
-      <button onClick={handleClick}>Get Balance</button>
+      <button
+        onClick={(event) => {
+          handleClick(event, 'coin');
+        }}
+      >
+        Get Balance
+      </button>
       <div> {balance}</div>
+      <button
+        onClick={(event) => {
+          handleClick(event, 'usdt');
+        }}
+      >
+        Get Balance USDT
+      </button>
+      <div> {tokenBalance}</div>
     </div>
   );
 };
